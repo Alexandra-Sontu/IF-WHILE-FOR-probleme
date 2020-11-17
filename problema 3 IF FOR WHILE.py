b02dea3a308797778757ac5d677355fd2069819b
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2020-11-17 16:47:21 +0300

    Add files via upload

---
+++ problema 3 IF FOR WHILE.py
@@ -0,0 +1,13 @@
+"""Se dau numerele m si n ,une m<n. Sa se verifice daca n este o putere a lui m."""
+import math
+n=int(input("dati un numar:"))
+m=int(input(*"dati un numar:"))
+if m<n:
+    l=math.log(n,m)
+    p=round(l)
+    if(m**p)==n:
+        print("da,n este putere a lui m")
+    else:
+        print("nu")
+
+
